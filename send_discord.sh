a3862118f6a74457f917f2465edf8026d0c3ae38
Sung-ho Kim <42758339+lancard@users.noreply.github.com>
2022-11-29 17:02:39 +0900

    Update send_discord.sh

--- send_discord.sh
+++ send_discord.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl \
-  -F 'payload_json={"username": "test", "content": "hello"}' \
+  -F 'payload_json={"content": "debug apk buil: "}' \
   -F "file1=@app/app-debug.apk" \
   $DISCORD_WEBHOOK
