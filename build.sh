b9fe95e5d1a04312b532c7194b667009ef4bd8e6
Sungho Kim <worstrocker@gmail.com>
2022-12-08 11:00:12 +0900

    passing GITHUB_RUN_NUMBER

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 docker run --rm \
 	-v $PWD/app:/app \
 	-v $PWD/keystore:/keystore \
+	-e GITHUB_RUN_NUMBER=$GITHUB_RUN_NUMBER \
 	-e KEYSTORE_PASSWORD=$1 \
 	lancard/android \
 	build
--- build_debug.sh
+++ build_debug.sh
@@ -2,5 +2,6 @@
 
 docker run --rm \
 	-v $PWD/app:/app \
+	-e GITHUB_RUN_NUMBER=$GITHUB_RUN_NUMBER \
 	lancard/android \
 	build_debug
